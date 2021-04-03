a790930843eef907c27a31939c9e613fee02e244
gabvrodrigues <gabriel.vieirarodrigues@gmail.com>
2021-04-03 14:59:24 -0300

    adding server

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express');
-const path = require('path');
 const serveStatic = require('serve-static');
+const path = require('path');
 
 const app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
-
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 80;
 app.listen(port);
-console.log(`server started ${port}`);
